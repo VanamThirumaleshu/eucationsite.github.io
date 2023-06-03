0d38df7cf1b27470497cdbb1938a4b893a2ccd8e
VanamThirumaleshu <126050981+VanamThirumaleshu@users.noreply.github.com>
2023-06-03 21:22:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+AOS.init({
+    duration : 1000
+});
+var typed=new Typed('#span-tag',{
+    strings : ["Your's Educational Institutions"],
+    typeSpeed : 100,
+    loop : true,
+    
+})
+hamburger=document.querySelector(".hamburger");
+hamburger.onclick=function(){
+    navBar=document.querySelector(".nav-bar");
+    navBar.classList.toggle("active");
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Get all the navigation links
+    var navLinks = document.getElementsByClassName('nav-link');
+  
+    // Add click event listener to each link
+    for (var i = 0; i < navLinks.length; i++) {
+      navLinks[i].addEventListener('click', function() {
+        // Remove 'active' class from all links
+        for (var j = 0; j < navLinks.length; j++) {
+          navLinks[j].classList.remove('active');
+        }
+  
+        // Add 'active' class to the clicked link
+        this.classList.add('active');
+        navBar.classList.remove("active");
+      });
+    }
+  });
